bb0124c0f8ae48275c46af07dc1fefe663031234
r.vazquez@meditecsols.com <r.vazquez@meditecsols.com>
2019-09-22 11:32:23 -0500

    test for tienda, POST and GET

--- commerce/tienda/tests.py
+++ commerce/tienda/tests.py
@@ -7,10 +7,10 @@ from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 from rest_framework import status
 
-from .models import Tienda
+from .models import Tienda, Dias, TiendaWorkingWindow
 
 
-class TiendaTests(TestCase):
+class TiendaCreationTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
@@ -75,3 +75,58 @@ class TiendaTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_json = response.json()
         self.assertNotEqual(response_json['slug'], 'tienda-de-martha-de-pasteles-caseros')
+
+
+class ObtainTiendaTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        tienda_data = {
+            'nombre': 'Tienda de Martha de pasteles Caseros',
+        }
+        response = self.client.post(
+                reverse('tienda-list'),
+                tienda_data,
+                format="json"
+            )
+        l = Dias.objects.create(valor=0, dia="Lunes")
+        m = Dias.objects.create(valor=1, dia="Martes")
+        mi = Dias.objects.create(valor=2, dia="Miércoles")
+        j =Dias.objects.create(valor=3, dia="Jueves")
+        v =Dias.objects.create(valor=4, dia="Viernes")
+        s =Dias.objects.create(valor=5, dia="Sábado")
+        d =Dias.objects.create(valor=6, dia="Domingo")
+
+        tienda = Tienda.objects.get(nombre=tienda_data['nombre'])
+
+        tw_one = TiendaWorkingWindow.objects.create(
+            tienda=tienda, 
+            start_time="09:00",
+            end_time="18:00"
+            )
+        tw_one.dias.add(l)
+        tw_one.dias.add(m)
+        tw_one.dias.add(mi)
+        tw_one.dias.add(j)
+        tw_one.dias.add(v)
+
+        tw_two = TiendaWorkingWindow.objects.create(
+            tienda=tienda, 
+            start_time="09:00",
+            end_time="18:00"
+            )
+        tw_two.dias.add(s)
+
+    def test_obtain_tienda(self):
+        """
+        Se obtiene GET de una tienda, 
+        que tiene horarios de trabajo
+        """
+
+        response = self.client.get(
+                reverse('tienda-list'),
+                format="json"
+        )
+
+        response_json = response.json()
+        self.assertEqual(len(response_json[0]['store_schedules']), 2) # se agregaron 2 working windows
