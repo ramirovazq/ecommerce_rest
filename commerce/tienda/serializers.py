acff34e27251e91ac7b529f4f06e386a9b2b8b0e
r.vazquez@meditecsols.com <r.vazquez@meditecsols.com>
2019-09-22 11:32:04 -0500

    necessary serializer for tienda, returns store_schedules

--- commerce/tienda/serializers.py
+++ commerce/tienda/serializers.py
@@ -1,10 +1,41 @@
 from rest_framework import serializers
-from .models import Tienda
+from .models import Tienda, Dias, TiendaWorkingWindow
 
-class TiendaSerializer(serializers.HyperlinkedModelSerializer):
+class DiasSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Dias
+        fields = (
+            'dia',
+        ) 
+
+class TiendaWorkingWindowSerializer(serializers.ModelSerializer):
+    dias = DiasSerializer(        
+        required=False, 
+        many=True
+    )
+
+    class Meta:
+        model = TiendaWorkingWindow
+        fields = (
+            'dias', 
+            'start_time',
+            'end_time'
+        ) 
+
+class TiendaSerializer(serializers.ModelSerializer):
+    store_schedules = serializers.SerializerMethodField()
+
+    def get_store_schedules(self, obj):        
+        schedules = TiendaWorkingWindow.objects.filter(tienda=obj)
+        tienda_working_window_serializer = TiendaWorkingWindowSerializer(schedules, many=True)
+        return tienda_working_window_serializer.data
+
     class Meta:
         model = Tienda
         fields = (
-        	'nombre', 
-        	'slug'
+            'id',
+            'nombre', 
+            'slug',
+            'store_schedules'
         ) 
