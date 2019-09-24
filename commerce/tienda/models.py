1842d1a3aa511ac23d1c0e7f3ae5697cf7ca21ad
r.vazquez@meditecsols.com <r.vazquez@meditecsols.com>
2019-09-24 18:34:53 -0500

    methods return date next_delivery_date

--- commerce/tienda/models.py
+++ commerce/tienda/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tienda.utils import lista_circular
 
 # Create your models here.
 class Tienda(models.Model):
@@ -18,20 +19,28 @@ class Tienda(models.Model):
     def horarios_laborales(self):
         return TiendaWorkingWindow.objects.filter(tienda=self)
 
-    def fecha_solicitada_dentro_de_horarios_laborales(self, fecha_a_checar):
+    def dias_semana_horarios_laborales(self):
+        answer = []
+        for x in self.horarios_laborales():
+            answer = answer + [y.valor for y in x.dias.all()]
+        answer = list(set(answer)) # quito dias repetidos
+        answer.sort()
+        return answer
+
+    def fecha_solicitada_dentro_de_dias_laborales(self, fecha_a_checar):
         answer = False
         for workingwindow in self.horarios_laborales():
-            print("--------------")
-            print("dia fecha_a_checar {}".format(fecha_a_checar.weekday()))
-            print("----dias_numeric----------")
-            print(workingwindow.dias_numeric())
             if fecha_a_checar.weekday() in workingwindow.dias_numeric(): # significa que cae dentro de un dia de los que se trabajan
                 answer = True
                 break
         return answer
 
-    def next_posible_day(self, today, fecha_de_entrega_validation):
-        return "recomendacion"
+    def next_posible_day(self, today, fecha_de_entrega_validation, dias_elaboracion):
+        lista_dias_trabajo = self.dias_semana_horarios_laborales()
+        dia_semana_que_pide = fecha_de_entrega_validation.weekday()
+        #lista_circular(lista_dias_trabajo=[0, 1, 3, 4], dia_que_pide=5, dias_elaboracion=0, fecha=None):
+        recomendation = lista_circular(lista_dias_trabajo, dia_semana_que_pide, dias_elaboracion, fecha_de_entrega_validation)
+        return recomendation
 
     def json_products_list_objects(self, list_of_dicts):
         from productos.models import Producto
@@ -47,17 +56,19 @@ class Tienda(models.Model):
         recibe la fecha de entrega enviada por el usuario
         recibe el json de los productos
         '''
-        print("recorriendo lista de productos ...............")
+        #print("recorriendo lista de productos ...............")
         list_answers = []
+        list_recomendations = []
         list_products = self.json_products_list_objects(products_json)
         for producto in list_products:
-            answer = self.validacion_en_horarios_laborales_producto(today, fecha_de_entrega_validation, producto)
+            answer, recomendation = self.validacion_en_horarios_laborales_producto(today, fecha_de_entrega_validation, producto)
             list_answers.append(answer)
+            list_recomendations.append(recomendation)
 
-        print("list_answers..............")
-        print(list_answers)
+        #print("list_answers..............")
+        #print(list_answers)
 
-        return not(False in list_answers)
+        return not(False in list_answers), list_recomendations
 
     def validacion_en_horarios_laborales_producto(self, today, fecha_de_entrega_validation, producto): #tienda.validacion_en_horarios_laborales(today, fecha_de_entrega_validation, products_json)
         '''
@@ -65,16 +76,16 @@ class Tienda(models.Model):
         recibe la fecha de entrega enviada por el usuario
         recibe un producto
         '''
-        print("validando un solo producto ...............")
+        #print("validando un solo producto ...............")
         answer = False
+        recomendation = None
         # productos con tiempo de elaboracion igual a cero, y el dia en que se solicita
-        if (producto.tiempo_de_elaboracion == 0):
-            answer = self.fecha_solicitada_dentro_de_horarios_laborales(fecha_de_entrega_validation)
-            if not(answer): # si no cae dentro de alguno de los dias laborales
-                recomendation = self.next_posible_day(today, fecha_de_entrega_validation)
-                print("recomendation.............................")
-                print(recomendation)
-        return answer
+
+        answer = self.fecha_solicitada_dentro_de_dias_laborales(fecha_de_entrega_validation)
+        if not(answer): # si no cae dentro de alguno de los dias laborales
+            recomendation = self.next_posible_day(today, fecha_de_entrega_validation, producto.tiempo_de_elaboracion)
+
+        return (answer, recomendation)
 
     def __str__(self):
         return u"{} id, nombre {}.".format(self.id, 
